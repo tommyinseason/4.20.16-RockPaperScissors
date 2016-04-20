fcc8b96f8ef6b553590942b672e81d47ddd6eccb
josh logan and tommy jones <josh+tommy@>
2016-04-20 09:09:08 -0700

    inital buid with file structure and no compiler errors

---
+++ src/main/java/RockPaperScissors.java
@@ -0,0 +1,9 @@
+public class RockPaperScissors {
+  public static void main(String[] args) {
+
+  }
+
+  public Boolean Game (String player1Play, String player2Play) {
+    return true;
+  }
+}
---
+++ src/test/java/RockPaperScissorsTest.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class RockPaperScissorsTest {
+
+  @Test
+  public void checkWinner_rockBeatsScissors_true() {
+
+  }
+
+}
